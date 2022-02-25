f92b0efcf8684f12d40a7941ff8501da891deb35
Christian Spath <christian.spath@datadialog.net>
2022-02-25 18:06:35 +0100

    Add AspNetIdentity

--- src/DaDiPortal.IdentityServer/Program.cs
+++ src/DaDiPortal.IdentityServer/Program.cs
@@ -1,3 +1,5 @@
+using DaDiPortal.IdentityServer.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace DaDiPortal.IdentityServer;
@@ -29,7 +31,15 @@ public static class Program
             .GetConnectionString("DefaultConnection");
 
         builder.Services
+            .AddDbContext<AspNetIdentityDbContext>(o => o.UseSqlServer(connStr, b => b.MigrationsAssembly(assemblyName)));
+
+        builder.Services
+            .AddIdentity<IdentityUser, IdentityRole>()
+            .AddEntityFrameworkStores<AspNetIdentityDbContext>();
+
+        builder.Services
             .AddIdentityServer()
+            .AddAspNetIdentity<IdentityUser>()
             .AddConfigurationStore(opt => opt.ConfigureDbContext = b => b.UseSqlServer(connStr, b1 => b1.MigrationsAssembly(assemblyName)))
             .AddOperationalStore(opt => opt.ConfigureDbContext = b => b.UseSqlServer(connStr, b1 => b1.MigrationsAssembly(assemblyName)))
             .AddDeveloperSigningCredential();
