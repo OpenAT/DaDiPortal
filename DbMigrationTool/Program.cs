dec47571eaca9800747eccbefd4ecfb7dbf8ef83
Christian Spath <christian.spath@datadialog.net>
2022-03-06 21:19:41 +0100

    Add project and build connection string

---
+++ DbMigrationTool/Program.cs
@@ -0,0 +1,44 @@
+﻿using DaDiPortal.IdentityServer.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DbMigrationTool;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var assemblyName = typeof(Program)
+            .Assembly
+            .GetName()
+            .Name;
+
+        var terminal = new Terminal()
+            .Info("Enter infos or press ctrl+c to quit\n")
+            .GetString("Database server", out string dbServer)
+            .GetString("Database name", out string dbName);
+
+        string connStr = GetConnStr(dbServer, dbName);
+
+        var services = new ServiceCollection()
+            .AddDbContext<AspNetIdentityDbContext>(o => o.UseSqlServer(connStr, b => b.MigrationsAssembly(assemblyName)))
+            .AddIdentityServer()
+            .AddConfigurationStore(opt => opt.ConfigureDbContext = b => b.UseSqlServer(connStr, b1 => b1.MigrationsAssembly(assemblyName)))
+            .AddOperationalStore(opt => opt.ConfigureDbContext = b => b.UseSqlServer(connStr, b1 => b1.MigrationsAssembly(assemblyName)));
+        
+        terminal.Info($"Applying migrations to:\n{connStr}");
+        terminal.Info("Press key to quit");
+        Console.ReadKey();
+    }
+
+    private static string GetConnStr(string dbServer, string dbName)
+    {
+        return new SqlConnectionStringBuilder
+        {
+            DataSource = dbServer,
+            InitialCatalog = dbName,
+            IntegratedSecurity = true
+        }.ToString();
+    }
+}
---
+++ DbMigrationTool/Terminal.cs
@@ -0,0 +1,28 @@
+﻿namespace DbMigrationTool;
+
+internal class Terminal
+{
+    internal Terminal GetString(string text, out string result)
+    {
+        Console.Write($"{text}:");
+        result = Console.ReadLine()!;
+
+        return this;
+    }
+
+    internal Terminal Info(string info)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine(info);
+
+        return this;
+    }
+
+    internal Terminal Error(string error)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(error);
+
+        return this;
+    }
+}
